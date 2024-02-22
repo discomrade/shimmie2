ec633fc4de7a7d5ac11e0b2d9bceaada67168c16
discomrade <83621080+discomrade@users.noreply.github.com>
2024-02-22 11:38:37 +0000

    Add leftybooru logo to home page

--- ext/home/theme.php
+++ ext/home/theme.php
@@ -48,7 +48,7 @@ class HomeTheme extends Themelet
             $page->body_attrs(),
             rawHTML("
 		<div id='front-page'>
-			<h1><a style='text-decoration: none;' href='".make_link()."'><span>$sitename</span></a></h1>
+			<div><a style='text-decoration: none;' href='".make_link()."'><img id='logo' src='/_images/95faa00405e06b0cf517a52f3af3c225.png' width='373' height='420' alt='leftybooru' /></a></div>
 			$main_links_html
 			$search_html
 			$message_html

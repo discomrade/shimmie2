3e1d297b7865ec58fa911c7ba48942f50a8ef24a
discomrade <83621080+discomrade@users.noreply.github.com>
2024-06-06 08:59:28 +0000

    [yotsuba] add title to view page

--- themes/yotsuba_b/view.theme.php
+++ themes/yotsuba_b/view.theme.php
@@ -15,7 +15,7 @@ class Yotsuba_BViewPostTheme extends ViewPostTheme
      */
     public function display_page(Image $image, array $editor_parts): void
     {
-        Ctx::$page->set_heading($image->get_tag_list());
+        Ctx::$page->set_title("#{$image->id}: ".$image->get_tag_list());
         Ctx::$page->add_block(new Block("Search", $this->build_navigation($image), "left", 0));
         Ctx::$page->add_block(new Block("Information", $this->build_stats($image), "left", 15));
         Ctx::$page->add_block(new Block(null, $this->build_info($image, $editor_parts), "main", 15));

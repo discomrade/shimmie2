76a86dc557b165e3b5a57d021177737c1e5deb73
discomrade <83621080+discomrade@users.noreply.github.com>
2025-02-11 06:18:35 +0000

    [holiday] replace april fools flip with alunya jneko
    credit: https://github.com/evert/jneko

---
+++ ext/holiday/javascript/jneko.js
@@ -0,0 +1,192 @@
+"use strict";
+
+const imageNames = [
+  'awake',
+//  'down1',
+//  'down2',
+//  'dtogi1',
+//  'dtogi2',
+//  'dwleft1',
+//  'dwleft2',
+  'jare2',
+  'kaki1',
+  'kaki2',
+//  'left1',
+//  'left2',
+//  'ltogi1',
+//  'ltog2',
+  'mati2',
+  'mati3',
+//  'right1',
+//  'right2',
+//  'rtogi1',
+//  'rtogi2',
+  'sleep1',
+  'sleep2',
+//  'up1',
+//  'up2',
+//  'upleft1',
+//  'upleft2',
+//  'upright1',
+//  'upright2',
+//  'utogi1',
+//  'utogi2',
+];
+const nekoSize = 64; 
+const images = Object.fromEntries(imageNames.map(name => {
+  const image = new Image(nekoSize, nekoSize);
+  image.src = '/ext/holiday/media/jneko/' + name + '.png';
+  return [name, image]
+}));
+
+
+
+function main() {
+
+  const headerH1 = document.querySelector('h1');
+  const nekoSpan = document.createElement('span');
+  nekoSpan.id = 'jneko';
+  headerH1.appendChild(nekoSpan);
+  const neko = new Neko(nekoSpan);
+
+}
+
+window.addEventListener('DOMContentLoaded', () => main());
+
+const stateMachine = {
+
+  // Name of the state
+  sleep: {
+
+    // Which images are used for the state
+    image: ['sleep1', 'sleep2'],
+
+    // How quickly we loop through these images
+    imageInterval: 1,
+
+    // What state does this go to when clicked
+    click: 'awake'
+  },
+  awake: {
+    image: 'awake',
+
+    // We automaticall transition to this state
+    nextState: 'normal',
+
+    // How long it takes to transition
+    nextStateDelay: 2.5,
+  },
+  normal: {
+    image: 'mati2',
+
+    // If there's multiple nextState values, a random one is picked.
+    // You make make 1 state more likely by addding it multiple times
+    // to the array
+    nextState: ['normal', 'normal', 'normal', 'tilt', 'scratch', 'yawn'],
+    nextStateDelay: 1.5,
+  },
+  tilt: {
+    image: 'jare2',
+    nextState: 'normal',
+    nextStateDelay: 1,
+  },
+  yawn: {
+    image: 'mati3',
+    nextState: ['normal', 'normal', 'sleep'],
+    nextStateDelay: 1,
+  },
+  scratch: {
+    image: ['kaki1', 'kaki2'],
+    imageInterval: 0.1,
+    nextState: 'normal',
+    nextStateDelay: 3,
+  }
+};
+
+class Neko {
+
+  constructor(elem) {
+
+    // The HTML element that hosts neko
+    this.elem = elem;
+    this.stateMachine = stateMachine;
+
+    // Creating a new <img> element
+    this.imgElem =  new Image(nekoSize, nekoSize);
+
+    elem.appendChild(this.imgElem);
+    this.imgElem.addEventListener('click', () => this.onClick());
+
+    this.setState('sleep');
+
+  }
+
+  /**
+   * This property is used to keep track of states with multiple frames
+   */
+  #animationIndex = 0;
+
+  renderImage() {
+
+    let name = this.stateMachine[this.#state].image;
+
+    this.#animationIndex++;
+    if (Array.isArray(name)) {
+      name = name[this.#animationIndex % name.length];
+    }
+    console.log('Rendering %s', name);
+    this.imgElem.src = images[name].src;
+  }
+
+  #state = null;
+  #nextStateTimeout = null;
+  #imageCycleInterval = null;
+
+  setState(stateName) {
+
+    clearTimeout(this.nextStateTimeout);
+    clearInterval(this.imageCycleInterval);
+
+    if (Array.isArray(stateName)) {
+      // If stateName was supplied as an array of strings, we'll randomly
+      // pick a new state.
+      stateName = stateName[Math.floor(Math.random()*(stateName.length))];
+    }
+    if (!this.stateMachine[stateName]) {
+      throw new Error('Uknown state: ' + stateName);
+    }
+    this.#state = stateName;
+    const stateData = this.stateMachine[this.#state];
+
+    // If there was a nextState, we automatically transition there after
+    // a delay.
+    if (stateData.nextState) {
+      this.nextStateTimeout = setTimeout(
+        () => this.setState(stateData.nextState),
+        stateData.nextStateDelay * 1000
+      );
+    }
+
+    // This block is responsible for cycling through multiple images
+    // of the current state.
+    if (stateData.imageInterval) {
+
+      this.imageCycleInterval = setInterval(
+        () => this.renderImage(),
+        stateData.imageInterval*1000
+      );
+
+    }
+    this.renderImage();
+  }
+  onClick() {
+    const stateData = this.stateMachine[this.#state];
+    if (stateData.click) {
+      // If the current state had a 'click' property, we'll transition
+      // to that state, otherwise it's ignored.
+      this.setState(stateData.click);
+    }
+
+  }
+
+}
--- ext/holiday/main.php
+++ ext/holiday/main.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-use function MicroHTML\LINK;
+use function MicroHTML\{LINK,SCRIPT};
 
 class Holiday extends Extension
 {
@@ -17,18 +17,22 @@ class Holiday extends Extension
     public function onSetupBuilding(SetupBuildingEvent $event): void
     {
         $sb = $event->panel->create_new_block("Holiday Theme");
-        $sb->add_bool_option("holiday_aprilfools", "Enable April Fools");
+        $sb->add_bool_option("holiday_aprilfools", "Enable Alunya neko on April Fools & May Day");
     }
 
     public function onPageRequest(PageRequestEvent $event): void
     {
         global $config, $page;
-        if (date('d/m') == '01/04' && $config->get_bool("holiday_aprilfools")) {
+        if ((date('d/m') == '01/04' || date('d/m') == '01/05') && $config->get_bool("holiday_aprilfools")) {
+            $page->add_html_header(SCRIPT([
+                'src' => get_base_href() . '/ext/holiday/javascript/jneko.js',
+            ]));
             $page->add_html_header(LINK([
                 'rel' => 'stylesheet',
-                'href' => get_base_href() . '/ext/holiday/stylesheets/aprilfools.css',
+                'href' => get_base_href() . '/ext/holiday/stylesheets/jneko.css',
                 'type' => 'text/css'
             ]));
+
         }
     }
 }
